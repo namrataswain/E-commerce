5fb65fe59500fb81aa14ddf89069a0ac95eb50c5
namrataswain456@gmail.com <namrataswain456@gmail.com>
2020-07-21 12:25:34 +0530

    adding react router for navigation among the different pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/homepage/homepage.components';
+
+const HatsPage = () =>(
+  <div>
+  <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+    <Switch>
+    <Route exact path='/' component={HomePage}/>
+      <Route  path='/hats' component = {HatsPage}/>
+    </Switch>
+      
     </div>
   );
 }
--- src/Components/directory/directory.components.jsx
+++ src/Components/directory/directory.components.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MenuItem } from '../menu-item/menu-tem.components';
+import { MenuItem } from '../menu-item/menu-item.components';
 import './directory.styles.scss';
 
 export class Directory extends React.Component {
