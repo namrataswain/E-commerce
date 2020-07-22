d474931bed78be7efbffbf1c839dc8d0d884c464
namrataswain456@gmail.com <namrataswain456@gmail.com>
2020-07-22 22:03:52 +0530

    Building the shop main page using collection preview component

--- src/App.js
+++ src/App.js
@@ -2,19 +2,15 @@ import React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/homepage/homepage.components';
+import ShopPage from './Pages/shop/shop.component';
 
-const HatsPage = () =>(
-  <div>
-  <h1>HATS PAGEs</h1>
-  </div>
-)
 
 function App() {
   return (
     <div className="App">
     <Switch>
     <Route exact path='/' component={HomePage}/>
-      <Route  path='/shop/hats' component = {HatsPage}/>
+      <Route exact path='/shop' component = {ShopPage}/>
     </Switch>
       
     </div>
