874b173121a1095f4d72007713e458c7ff2ecedb
namrataswain456@gmail.com <namrataswain456@gmail.com>
2020-07-24 13:21:01 +0530

    Integrated firebase authentication, sign in with google enabled with proper styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import React from 'react';
-import {Switch, Route} from 'react-router-dom';
-import './App.css';
-import HomePage from './Pages/homepage/homepage.components';
-import ShopPage from './Pages/shop/shop.component';
-import SignInAndSignUpPage from './Pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import Header from './Components/Header/header-component';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import HomePage from "./Pages/homepage/homepage.components";
+import ShopPage from "./Pages/shop/shop.component";
+import SignInAndSignUpPage from "./Pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import Header from "./Components/Header/header-component";
+import { auth } from "./firebase/firebase.utils";
 
-function App() {
-  return (
-    <div className="App">
-    <Header/>
-    <Switch>
-    <Route exact path='/' component={HomePage}/>
-      <Route exact path='/shop' component = {ShopPage}/>
-      <Route exact path='/signin' component = {SignInAndSignUpPage}/>
-    </Switch>
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser : null
+    }
+  }
+
+   unsubscribeFromAuth = null;
+
+  componentDidMount() {
+
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({
+        currentUser: user
+      });
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth ();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/sign-in/sign-in.component.jsx
+++ src/Components/sign-in/sign-in.component.jsx
@@ -60,9 +60,11 @@ class SignIn extends React.Component {
             required
             label ='Password'
             />
-            
-          <CustomButton type ='submit'>Sign In</CustomButton>
-          <CustomButton onClick ={signInWithGoogle}>Sign in with Google {''}</CustomButton>
+            <div className='buttons'>
+            <CustomButton type ='submit'>Sign In</CustomButton>
+            <CustomButton isGoogleSignIn onClick ={signInWithGoogle}>Sign in with Google {''}</CustomButton>
+            </div>
+         
            
             </form>
             </div>
