d9666b15216f55a45e76d143a42ad1c26946c281
namrataswain456@gmail.com <namrataswain456@gmail.com>
2020-07-25 09:29:43 +0530

    Added the function to store the user's data into the firestore

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import HomePage from "./Pages/homepage/homepage.components";
 import ShopPage from "./Pages/shop/shop.component";
 import SignInAndSignUpPage from "./Pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./Components/Header/header-component";
-import { auth } from "./firebase/firebase.utils";
+import { auth , createUserProfileDocument} from "./firebase/firebase.utils";
+
 
 class App extends React.Component {
   constructor(props) {
@@ -20,12 +21,9 @@ class App extends React.Component {
 
   componentDidMount() {
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({
-        currentUser: user
-      });
-
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async user => {
+      createUserProfileDocument(user); //storing the data of the user into the firestore
+      //next is to think about storing data into the state of the component of our app
     })
   }
 
