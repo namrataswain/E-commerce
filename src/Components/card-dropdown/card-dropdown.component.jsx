629c2eba6fb0af987142a031bc89a55c0dee4983
namrataswain456@gmail.com <namrataswain456@gmail.com>
2020-07-30 18:06:02 +0530

    hide cart dropdown if the checkout page is rendered using dispatch shorthand

--- src/Components/card-dropdown/card-dropdown.component.jsx
+++ src/Components/card-dropdown/card-dropdown.component.jsx
@@ -5,10 +5,11 @@ import {connect} from 'react-redux';
 import { selectCartItems} from '../../redux/cart/cart.selectors.js';
 import {createStructuredSelector} from 'reselect';
 import {withRouter} from 'react-router-dom';
+import { toggleCartHidden } from '../../redux/cart/cart.action.js';
 import './card-dropdown.styles.scss';
 
 
-const CartDropdown = ({cartItems, history}) => (
+const CartDropdown = ({cartItems, history, dispatch}) => (
   <div className='cart-dropdown'>
     <div className ='cart-items'>
     
@@ -23,13 +24,17 @@ const CartDropdown = ({cartItems, history}) => (
 
     </div>
    
-      <CustomButton onClick = {() => history.push('/checkout')}>GO TO CHECKKOUT</CustomButton>
+      <CustomButton  onClick = {() => {
+        history.push('/checkout');
+        dispatch(toggleCartHidden());
+
+      }}>GO TO CHECKKOUT</CustomButton>
    
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  cartItems : selectCartItems
+  cartItems : selectCartItems,
 })
 
 export default withRouter( connect(mapStateToProps)(CartDropdown));
