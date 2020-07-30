c219ceace6ce6c0f177c113f3665db13a582e364
namrataswain456@gmail.com <namrataswain456@gmail.com>
2020-07-30 19:00:22 +0530

    remove item using the X symbol on the checkout item component

--- src/Pages/checkout/checkout.component.jsx
+++ src/Pages/checkout/checkout.component.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-import {selectCartItems, selectCartTotal} from "../../redux/cart/cart.selectors.js";
-import CheckoutItem from '../../Components/checkout-item/checkout-item.component';
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../redux/cart/cart.selectors.js";
+import CheckoutItem from "../../Components/checkout-item/checkout-item.component";
 import "./checkout.styles.scss";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
     <div className="checkout-header">
-      
       <div className="header-block">
         <span>Product</span>
       </div>
@@ -26,9 +28,9 @@ const CheckoutPage = ({ cartItems, total }) => (
       </div>
     </div>
 
-    {cartItems.map((cartItem) => 
-        <CheckoutItem key ={cartItem.id} cartItem= {cartItem}/> 
-        )}
+    {cartItems.map((cartItem) => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
     <div className="total">
       <span>Total=${total}</span>
     </div>
