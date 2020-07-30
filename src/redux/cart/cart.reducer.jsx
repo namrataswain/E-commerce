6041de21acc85c211d9ad3918fbeb898435978a9
namrataswain456@gmail.com <namrataswain456@gmail.com>
2020-07-30 20:57:49 +0530

    all the icons are functional with appropriate  onClick method in checkout-item component

--- src/redux/cart/cart.reducer.jsx
+++ src/redux/cart/cart.reducer.jsx
@@ -1,5 +1,6 @@
 import CartActionTypes from './cart.types';
-import {addItemToCart} from './cart.utilis';
+import {addItemToCart, removeItemFromCart} from './cart.utilis';
+
 
 
 const INITIAL_STATE = {
@@ -19,11 +20,17 @@ const cartReducer = (state = INITIAL_STATE, action) =>{
                 ...state,
                 cartItems : addItemToCart(state.cartItems, action.payload)
             }
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            }
         case CartActionTypes.CLEAR_ITEM_FROM_CART:
             return{
                 ...state,
                 cartItems : state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
             }
+        
         default:
             return state
     }
