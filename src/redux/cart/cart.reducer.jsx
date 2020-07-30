6637fc7c4bf6fbeb62c996162a9e3632f51bef90
namrataswain456@gmail.com <namrataswain456@gmail.com>
2020-07-30 14:55:02 +0530

    cart item action added to the cart reducer

--- src/redux/cart/cart.reducer.jsx
+++ src/redux/cart/cart.reducer.jsx
@@ -2,7 +2,8 @@ import CartActionTypes from './cart.types'
 
 
 const INITIAL_STATE = {
-    hidden : true // dropdown should be invisible as the user should not see when they visit the site
+    hidden : true, // dropdown should be invisible as the user should not see when they visit the site
+    cartItems : []
 }
 
 const cartReducer = (state = INITIAL_STATE, action) =>{
@@ -12,6 +13,11 @@ const cartReducer = (state = INITIAL_STATE, action) =>{
             ...state,
             hidden : !state.hidden //no payload 
         }
+        case CartActionTypes.ADD_ITEM:
+            return{
+                ...state,
+                cartItems : [...state.cartItems, action.payload]
+            }
         default:
             return state
     }
