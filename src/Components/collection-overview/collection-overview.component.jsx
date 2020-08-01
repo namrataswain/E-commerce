fb55cbe14bd16b769d89fb30d7dd25b658cd6764
namrataswain456@gmail.com <namrataswain456@gmail.com>
2020-08-01 10:49:37 +0530

    refactored the shop data to acheieve data normalisation by changing it into an object instead of an array of objects

--- src/Components/collection-overview/collection-overview.component.jsx
+++ src/Components/collection-overview/collection-overview.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
-import {selectShopCollections} from '../../redux/shop/shop.selectors';
+import {selectCollectionForPreview} from '../../redux/shop/shop.selectors';
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
 import './collection-overview.styles.scss';
@@ -19,7 +19,7 @@ const CollectionsOverview = ({collections}) => (
 );
 
 const mapStateToProps = createStructuredSelector({
-    collections : selectShopCollections
+    collections : selectCollectionForPreview 
 })
 
 
