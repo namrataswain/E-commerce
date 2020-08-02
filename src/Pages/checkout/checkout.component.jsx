1123a566c6c45375cf23511972abe6eb4f580754
namrataswain456@gmail.com <namrataswain456@gmail.com>
2020-08-02 21:35:30 +0530

    Integrated stripe button in our own component and used it in checkout page component

--- src/Pages/checkout/checkout.component.jsx
+++ src/Pages/checkout/checkout.component.jsx
@@ -7,6 +7,7 @@ import {
 } from "../../redux/cart/cart.selectors.js";
 import CheckoutItem from "../../Components/checkout-item/checkout-item.component";
 import "./checkout.styles.scss";
+import StripeCheckoutButton from "../../Components/stripe-button/stripe-button.component.jsx";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -34,6 +35,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>Total=${total}</span>
     </div>
+    <div className='test-warning'>
+    *Please use the following test credit card for payments*
+    <br/>
+    4242 4242 4242 4242 - Exp : 01/21 - CVV - 123
+    </div>
+    <StripeCheckoutButton price = {total}/>
   </div>
 );
 
